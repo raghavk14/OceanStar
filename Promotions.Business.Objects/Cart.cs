3b10d15015daebd46c8ecd68eeb0d7e0a0de5092
Raghavendra Kammar <raghu_tex@yahoo.com>
2020-07-03 18:51:09 -0700

    Making changes to cart and Promotion addition tests

--- Promotions.Business.Objects/Cart.cs
+++ Promotions.Business.Objects/Cart.cs
@@ -1,9 +1,80 @@
-﻿using System;
+﻿
 
-namespace Promotions.Business.Objects
-{
-    public class Cart
+
+   
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    namespace Promotion.Business.Objects
     {
-        public int Count { get; set; } = 0;
+        public class Cart
+        {
+            protected Dictionary<Product, int> items = new Dictionary<Product, int>();
+
+            public ProductPromotionStrategy PromotionStrategy { get; set; }
+
+            public int Count
+            {
+                get
+                {
+                    return this.items.Count;
+                }
+            }
+
+            public void AddItem(Product newItem)
+            {
+                if (!items.ContainsKey(newItem))
+                    items.Add(newItem, 1);
+                else
+                    items[newItem]++;
+
+            }
+            public void RemoveItem(Product item)
+            {
+                items.Remove(item);
+            }
+
+            public double GetCartTotal()
+            {
+                //if(PromotionStrategy == null || PromotionStrategy.GetType() == typeof(NoPromotion))
+                if (PromotionStrategy == null)
+                {
+                    PromotionStrategy = GetNoPromotionRuleSet();
+                }
+                //return PromotionRule.Apply(items);
+                return PromotionStrategy.Apply(items);
+            }
+
+            public string Receipt() { return this.ToString(); }
+
+            public override string ToString()
+            {
+                string reciept = "";
+
+                foreach (var item in items)
+                {
+                    reciept += item.ToString() + "\n";
+                }
+
+                reciept += "\n\n Total: " + GetCartTotal();
+                return reciept;
+            }
+
+            public ProductPromotionStrategy GetNoPromotionRuleSet()
+            {
+                var promoRuleList = new List<ProductPromoter>();
+                //Get distinct sku items from the cart.
+                foreach (var skutype in Enum.GetNames(typeof(SKUType)))
+                {
+                    var product = items.Where(sku => sku.Key.SkuType.ToString() == skutype).Select(sku => sku.Key).FirstOrDefault();
+                    //For each sku type add "no product promotion rule".
+                    if (product != null)
+                        promoRuleList.Add(new ProductPromoter(product.SkuType, product.Price, 1, product.Price, 1));
+                }
+
+                return new NoPromotion(promoRuleList);
+            }
+        }
     }
-}
+
---
+++ Promotions.Business.Objects/Promotion.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Promotion.Business.Objects
+{
+    public class Promotion
+    {
+        public IPromotion PriceRule { get; set; }
+
+        private Promotion() { }
+
+        public Promotion(IPromotion priceRule)
+        {
+            PriceRule = priceRule;
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var thisRule in PriceRule.SKURules)
+            {
+                var discount = string.Empty;
+                if (thisRule.Discount != 1)
+                {
+                    stringBuilder.Append(thisRule.Count + " " + thisRule.SkuType + " @" + thisRule.Discount + "% Off");
+                }
+                else
+                {
+                    stringBuilder.Append(thisRule.Count + " " + thisRule.SkuType + " @" + thisRule.PromotionPrice);
+                }
+
+            }
+            return stringBuilder.ToString();
+        }
+
+        public double Apply(Dictionary<Product, int> items)
+        {
+            //Iterate through cart items and apply the promotion rule.
+            return PriceRule.Apply(items);
+        }
+    }
+}
