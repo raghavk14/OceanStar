bb4ce56c8d866ae61ea4796adcc0bc04d0731b40
Raghavendra Kammar <raghu_tex@yahoo.com>
2020-07-03 18:29:08 -0700

    Adding tests for flat price promotions

--- PromotionsEngine.Tests/PromotionEngineTestSet.cs
+++ PromotionsEngine.Tests/PromotionEngineTestSet.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using PR = Promotion.Business.Objects;
 
@@ -30,6 +31,42 @@ namespace PromotionsEngine.Tests
             Assert.Throws<Exception>(() => new PR.ProductPromoter(PR.SKUType.B, 10, 30, 0, 1));
 
         }
+        //2.Add a sku type to the flat price promotion rule
+        [Fact]
+        public void OneTypeSkuFlatPricePromotionTest()
+        {
+            var cart = new PR.Cart();
+            var product = new PR.Product(PR.SKUType.A, 50);
+            cart.AddItem(product);
+            var flatPriceRule = new PR.ProductPromoter(product.SkuType, product.Price, 3, 130, 1);
+            var promoRuleList = new List<PR.ProductPromoter>();
+            promoRuleList.Add(flatPriceRule);
+
+            //PR.IPromotion promotion = new PR.FlatPricePromotion(promoRuleList);
+            PR.ProductPromotionStrategy promotion = new PR.FlatPricePromotion(promoRuleList);
+            Assert.Equal(" 3 A(s) @ 130$", promotion.ToString());
+        }
+        //3.Add multiple type skus to same flat price promotion rule
+        [Fact]
+        public void Promotion_AddMultipleSkuTest()
+        {
+            var cart = new PR.Cart();
+            var productA = new PR.Product(PR.SKUType.A, 50);
+            var productB = new PR.Product(PR.SKUType.B, 30);
+            cart.AddItem(productA);
+            for (int i = 0; i < 2; i++)
+                cart.AddItem(productB);
+
+            var flatPriceRule1 = new PR.ProductPromoter(productA.SkuType, productA.Price, 1, 50, 1);
+            var flatPriceRule2 = new PR.ProductPromoter(productB.SkuType, productB.Price, 2, 30, 1);
+            var promoRuleList = new List<PR.ProductPromoter>();
+            promoRuleList.Add(flatPriceRule1);
+            promoRuleList.Add(flatPriceRule2);
+
+            PR.ProductPromotionStrategy promotion = new PR.FlatPricePromotion(promoRuleList);
+            Assert.Equal(" 1 A @ 50$ 2 B(s) @ 30$", promotion.ToString());
+        }
+
     }
-    
+
 }
