18587a0425f2b983c3b78d6e213e934e580e5f52
Raghavendra Kammar <raghu_tex@yahoo.com>
2020-07-03 17:54:03 -0700

    Add void promotion = Null object pattern

---
+++ Promotions.Business.Objects/NoPromotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Promotion.Business.Objects
+{
+    //Null object pattern
+    public class NoPromotion : FlatPricePromotion
+    {
+
+        public NoPromotion(List<ProductPromoter> skuRules) : base(skuRules)
+        {
+
+        }
+
+        public override double CalculatePrice(int itemCount, ProductPromoter currentRule) => base.CalculatePrice(itemCount, currentRule);
+
+
+        public override string ToString() => base.ToString();
+
+    }
+
+}
