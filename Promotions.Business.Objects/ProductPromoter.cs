1348aa06d786ebf1a1de1fbad9a4b7a45523d9d4
Raghavendra Kammar <raghu_tex@yahoo.com>
2020-07-03 18:10:48 -0700

    Adding Promotion strategy with abstract implementations

---
+++ Promotions.Business.Objects/ProductPromoter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Promotion.Business.Objects
+{
+    public class ProductPromoter : Product
+    {
+        private int _count;
+        private double _promoPrice;
+
+        public double PromotionPrice
+        {
+            get { return _promoPrice; }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Invalid Promotion Price");
+                }
+                _promoPrice = value;
+
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+
+            set
+            {
+                if (value < 1)
+                {
+                    throw new Exception("Invalid Count for this Product Promotion");
+                }
+                _count = value;
+            }
+        }
+
+        public double ProudctPrice
+        {
+            get { return base.Price; }
+            set { base.Price = value; }
+        }
+
+        public double Discount { get; set; } = 1;
+
+        public ProductPromoter(SKUType sKUType, double productPrice, int count, double promotionPrice, double discount) : base(sKUType, productPrice)
+        {
+            PromotionPrice = promotionPrice;
+            Count = count;
+            Discount = discount;
+        }
+
+
+    }
+}
+
---
+++ Promotions.Business.Objects/ProductPromotionStrategy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Promotion.Business.Objects
+{
+    public abstract class ProductPromotionStrategy
+    {
+        //Instantiate necessary members thro' abstract constructor
+        protected ProductPromotionStrategy(List<ProductPromoter> skuRules)
+        {
+            SKURules = skuRules;
+        }
+
+        protected List<ProductPromoter> SKURules { get; set; }
+
+        public abstract double CalculatePrice(int itemCount, ProductPromoter currentRule);
+
+        public virtual double Apply(Dictionary<Product, int> cartItems)
+        {
+            var totalPrice = 0.0;
+            //First create groups based on sku type of the product
+            var productGroups = GetProductsGroupedBySkuType(cartItems);
+
+            //Then for each group apply the promotion rule of that SKUType
+            foreach (var currentGroup in productGroups)
+            {
+                var itemCount = currentGroup.First().Value;
+                var currentRule = SKURules.Where(p => p.SkuType == currentGroup.Key).FirstOrDefault();
+                totalPrice += CalculatePrice(itemCount, currentRule);
+            }
+
+            //Return total price of this group
+
+            return totalPrice;
+        }
+
+        protected IOrderedEnumerable<IGrouping<SKUType, KeyValuePair<Product, int>>> GetProductsGroupedBySkuType(Dictionary<Product, int> cartItems)
+        {
+            var productGroups = from item in cartItems
+                                group item by item.Key.SkuType into prodGroup
+                                orderby prodGroup.Key
+                                select prodGroup;
+            return productGroups;
+        }
+    }
+
+
+}
