a91bf3c88430b69f82e8279a6baf6082fa80b554
Raghavendra Kammar <raghu_tex@yahoo.com>
2020-07-03 17:52:25 -0700

    Adding product type enum and Promotion type

---
+++ Promotions.Business.Objects/IPromotion.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Promotion.Business.Objects
+{
+    public interface IPromotion
+    {
+        List<ProductPromoter> SKURules { get; set; }
+        double Apply(Dictionary<Product, int> cartItems);
+    }
+}
---
+++ Promotions.Business.Objects/SKUType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Promotion.Business.Objects
+{
+    public enum SKUType
+    {
+        A = 1,
+        B = 2,
+        C = 3,
+        D = 4
+    }
+}
