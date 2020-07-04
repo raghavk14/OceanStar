1f46fad06bbaa49bf84bed943ccc2c2ba5be996d
Raghavendra Kammar <raghu_tex@yahoo.com>
2020-07-03 18:15:12 -0700

    Adding some exception test cases for promotions

--- PromotionsEngine.Tests/PromotionEngineTestSet.cs
+++ PromotionsEngine.Tests/PromotionEngineTestSet.cs
@@ -1,14 +1,35 @@
 using System;
 using Xunit;
+using PR = Promotion.Business.Objects;
 
 namespace PromotionsEngine.Tests
 {
     public class PromotionEngineTestSet
     {
-        [Fact]
+       
+       //1.Create a promotion rule
+       [Fact]
         public void Promotion_CreateTest()
         {
+            var promotion = new PR.FlatPricePromotion(null);
+            Assert.IsAssignableFrom<PR.ProductPromotionStrategy>(promotion);
+        }
+
+        //Invalid Product promoter, throws exception
+        [Fact]
+        public void ZeroProductCount_ThrowsException_Test()
+        {
+            Assert.Throws<Exception>(() => new PR.ProductPromoter(PR.SKUType.B, 40, 0, 25, 1));
+
+        }
+
+        //Invalid Product promoter, throws exception
+        [Fact]
+        public void InvalidPromotionPrice_ThrowsException_Test()
+        {
+            Assert.Throws<Exception>(() => new PR.ProductPromoter(PR.SKUType.B, 10, 30, 0, 1));
 
         }
     }
+    
 }
