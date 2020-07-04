4c22d3820a4a662154510b247b85f80fbab7ba51
Raghavendra Kammar <raghu_tex@yahoo.com>
2020-07-03 17:45:59 -0700

    Testing Product addition with cart

---
+++ Promotions.Business.Objects/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Promotion.Business.Objects
+{
+    public class Product
+    {
+        private double _price;
+        public SKUType SkuType { get; set; }
+
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value <= 0.0)
+                {
+                    throw new Exception("Invalid product price");
+                }
+                _price = value;
+            }
+        }
+
+        public Product(SKUType skuType, double price)
+        {
+            SkuType = skuType;
+            Price = price;
+        }
+
+
+        public override bool Equals(object otherProduct)
+        {
+            return this.SkuType.Equals((otherProduct as Product).SkuType);
+
+        }
+
+        public override string ToString()
+        {
+            return this.GetType().Name + this.SkuType + " Priced @ " + this.Price;
+        }
+    }
+}
---
+++ PromotionsEngine.Tests/ProductTestSet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit;
+using PR = Promotion.Business.Objects;
+
+namespace PromotionEngine.Tests
+{
+    public class ProductTestSet
+    {
+        //create a product
+        [Fact]
+        public void CreateProductTest()
+        {
+            var product = new PR.Product(PR.SKUType.A, 1);
+            Assert.IsAssignableFrom<PR.Product>(product);
+            Assert.NotNull(product);
+        }
+        //print the product string
+        [Fact]
+        public void PrintProductTest()
+        {
+            var product = new PR.Product(PR.SKUType.A, 50);
+            Assert.IsAssignableFrom<PR.Product>(product);
+            Assert.NotNull(product);
+            Assert.Equal("ProductA Priced @ 50", product.ToString());
+        }
+
+        //Invalid Product price throw exception
+        [Fact]
+        public void InvalidProductPrice_Throws_Exception()
+        {
+            Assert.Throws<Exception>(() => new PR.Product(PR.SKUType.A, 0.0));
+
+        }
+        //compare two products for similarity
+
+        //compare two products for dissimilarity
+    }
+}
